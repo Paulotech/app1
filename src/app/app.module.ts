e57391b82eea4bc7621c27097939e959898af6bd
Paulo Hericles <paulo.hericles.silva@gmail.com>
2019-12-13 15:53:31 -0300

    App finalizado!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
-import { PainelComponent } from './painel/painel.component';
+import { PainelComponent } from "./painel/painel.component";
 import { TentativasComponent } from './tentativas/tentativas.component';
 import { ProgressoComponent } from './progresso/progresso.component';
 
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { Frase } from '../shared/frase.model';
 import { FRASES } from './frase-mock';
 
@@ -7,16 +7,63 @@ import { FRASES } from './frase-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
+  public resposta: string = ''
 
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
   
+  public tentativas: number = 3
 
-  constructor() { console.log(this.frases) }
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
+  constructor() { 
+    //Atualiza o objeto rodadaFrase
+    this.atualizaRodada()
+  }
 
   ngOnInit() {
+  }
+  ngOnDestroy() {
+  }
+  
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+  
+  public verificarResposta(): void {
+
+    if(this.rodadaFrase.frasePtBr == this.resposta) {
+      //Trocar pergunta da rodada
+      this.rodada++
+      
+      //Progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      
+      if(this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+      } 
+      this.atualizaRodada()
+    } else {
+      //Diminuir a variável tentativas
+      this.tentativas--
+
+      if(this.tentativas === -1) {
+        this.encerrarJogo.emit('derrota')
+      }
+    }
+  }
+
+  public atualizaRodada(): void {
+    //Atualiza o objeto rodadaFrase
+    this.rodadaFrase = this.frases[this.rodada]
+    //Limpa a resposta
+    this.resposta = ""
   }
 
 }
